be1ad1bfcfeffd9a23c33a85a576487b4be6d7b4
wanie5072 <missa_lastfera@yahoo.com>
2023-03-14 11:03:28 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hrappv2',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
